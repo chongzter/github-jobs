de204ac956c0329aa2da5e65e381a78de915b5c3
zackster <ftchong235@yahoo.com>
2020-07-24 17:16:29 +0800

    Updates

---
+++ precache-manifest.d8e7f1fe7e5578b946b6028fd3f875a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257bd8d0f0641d756fef00888c05293e",
+    "url": "/github-jobs/index.html"
+  },
+  {
+    "revision": "0c30500b8ead0abb82cc",
+    "url": "/github-jobs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0ecd759910e3dfa0c13e",
+    "url": "/github-jobs/static/js/2.beb04b6e.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github-jobs/static/js/2.beb04b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c30500b8ead0abb82cc",
+    "url": "/github-jobs/static/js/main.65d26b63.chunk.js"
+  },
+  {
+    "revision": "6826c85f25f9f6feef04",
+    "url": "/github-jobs/static/js/runtime-main.f25054b9.js"
+  }
+]);
